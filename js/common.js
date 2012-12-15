bd1153d55f45bed82b6b16abc499ca6de5c742ae
Brett Beers <beersbr@gmail.com>
2012-12-14 23:11:19 -0700

    First commit

---
+++ js/common.js
@@ -0,0 +1,11 @@
+// Request animation frame gist for canvas 
+requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+     window.webkitRequestAnimationFrame ||
+     window.mozRequestAnimationFrame ||
+     window.oRequestAnimationFrame ||
+     window.msRequestAnimationFrame ||
+     function(/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+       window.setTimeout(callback, 1000/60);
+     };
+})();
